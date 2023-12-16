0f7f88e067841b46be506e56f7d587007c325941
raphaelagil <raphaela.gil@yahoo.com>
2023-12-16 14:08:00 +0000

    Added confirmation displaying the chosen length and characters

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Password-generator
 
-# Console-Finances
-
 ## Overview
-This project marks the 4th challenge in my Front-End Web Developer Bootcamp, where we've learned JavaScript fundamentals. Through this challenge, I've gained practical knowledge in various fundamental concepts including variables, primitive types, logical comparison operators, conditional statements, arrays, user interaction using prompts, and implementation of loops like the for loop.
+This project marks the 5th challenge in my Front-End Web Developer Bootcamp, where we've learned JavaScript fundamentals. Through this challenge, I've gained practical knowledge in various fundamental concepts including objects, objects methods, nested data, looping nested, functions, parameters, scope.
 
 ## Description
-The challenge involved creating a financial analysis program using JavaScript. The main goal was to determine the number of months included in the datasheet, the net total amount, the average of the changes of Profit/Losses over the entire period, the total amount month with the greatest decrease and increase in profits/losses. By employing a for loop, I iterated through the dataset, comparing consecutive months' profit/loss values to find the month with the most significant decrease and increase. I utilized conditional statements to track and update variables holding the greatest decrease and greatest increase, and corresponding month.
+The challenge entails creating a customized password based on the user's preferences and displaying it on a webpage. It involves a sequence of one prompts to specify the desired password length and four confirmation for the inclusion of numeric, uppercase, lowercase, and special characters. The password must be between 8 to 128 characters in length, and at least one character type must be selected. After specifying preferences, the password generator validates the selection to ensure at least one character type is chosen before generating the password. Upon clicking the button, the password will be displayed in the text area.
 
 ## Table of Contents
 - [Installation](#installation)
@@ -25,24 +23,30 @@ To set up and view this project locally, follow these simple steps:
 
 ## Screenshots
 ### Dashboard Overview
-![Raphaela Finances]()
+![Raphaela Password Generator](https://github.com/RaphaGil/Password-generator/assets/128820385/fb6ac2d8-3d31-4717-bc42-f68bbc89491c)
 *Caption: Overview of the project's main webpage.*
-### Console Output
-![Console](h)
-*Caption: Detailed view of the console output, presenting the calculated financial analysis results with specific data insights.*
-
+### Dashboard Overview with the generated password
+![Password Password Generator result](https://github.com/RaphaGil/Password-generator/assets/128820385/18835de9-f018-44b5-95d5-1a51e7c9f5b5)
+*Caption: A detailed view of the main webpage showcasing the generated password results.*
+![Password Password Generator prompt](https://github.com/RaphaGil/Password-generator/assets/128820385/5a54ac67-a40e-4b9a-9af2-afc8a84a560d)
+*Caption: A detailed view of the main webpage showcasing the prompt.*
 
 ## Usage
 - JavaScript is employed to dynamically display content or sections based on user interactions or navigation, providing a more interactive and user-friendly experience.
 
 ## Features
-- Utilizing JavaScript variables and understanding primitive types
+- Utilizing JavaScript functions and understanting it;
 - Implementing logical comparison operators for conditional evaluations
 - Employing conditional statements for decision-making processes
 - Working with arrays to store and manipulate data
 - Interacting with users through prompts to input data
+- Working with comments to details more information of the process and code
 - Applying loops, specifically the for loop, to iterate through data efficiently
-- Introducing and understanding the basic usage of the switch statement
+- Introducing and understanding the interative methods and it contribution in the coding as join,
+- Understanding the use of Math.floor(Math.random) to generate a new password
+- Undertande the logical of the functions and how they interact with themselves,
+- How to return more then one let in a object
+
 
 ## Contributing
 If you're also on the journey of learning web development and wish to contribute or offer feedback, don't hesitate to fork the repository and submit a pull request. Contributions and suggestions are highly valued!
@@ -54,6 +58,6 @@ If you're also on the journey of learning web development and wish to contribute
 ## License
 This project is licensed under the MIT License. The MIT License is an open-source license widely used for software projects. It is known for its permissive nature, allowing users to freely use, modify, distribute, and sublicense the software without significant legal restrictions.
 
-[Raphaela Gil_Finances]() - Raphaela Gil 
+[Raphaela Gil_Password-Generator](https://raphagil.github.io/Password-generator/) - Raphaela Gil 
 ## Contact Information
 If you're interested in sharing ideas, experiences, or collaborating on projects, please feel free to get in touch via email at raphaela.gil@yahoo.com or [![Linkedin](https://i.stack.imgur.com/gVE0j.png) LinkedIn](https://www.linkedin.com/in/raphaela-do-amaral-gil-0a9bb945/ ). You can also check my github profile at [![RaphaGil](https://i.stack.imgur.com/tskMh.png) GitHub](https://github.com/RaphaGil).
--- script.js
+++ script.js
@@ -1,161 +1,157 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
-
-// * Generate a password when the button is clicked
-//   * Present a series of prompts for password criteria
-    // * Length of password
-//       * At least 8 characters but no more than 128.
-//     * Character types
-//       * Lowercase
-//       * Uppercase
-//       * Numeric
-//       * Special characters ($@%&*, etc)
-//   * Code should validate for each input and at least one character type should be selected
-//   * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
-
-// Sample pseudocode for Password Generator:
-// const charOptions = [];
-// const generatedPassword = '';
-// You can store the generatedPassword as a string and concat each character OR
-// as an array and push each character, then join once you have enough characters
-
-// Function to prompt user for password options
-// function getPasswordOptions() {
-  // Prompt for password length
-  // At least 8 characters, no more than 128 characters
-  // Conditional to check that the number that was entered is in range
-  // Prompts store data as strings, so need to parse into a number
-  // If the user's input is out of range, either return out of the function or call the function again
-
-  // Confirm which character sets to use
-  // If the user answers false for all, either return out of the function or call the function again
-  
-  // Once they select a character set:
-  // Generate a random character for each selected character set
-  // Either push selected character sets to a mega-array of all selected characters
-  // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
-  
-  // Once character sets are selected, move on to generating random characters
-
-
-// Function for getting a random element from an array
-// function getRandom(arr) {
-  // Need a variable to hold the password as it's being generated
-  // Need a variable to hold the index that's being generated
-
-  // For loop that loops the number of times that matches the length the user chose
-  // Generate a random number
-  // That number is the index for a character in the mega-array
-  // So then, mega-array[generated-index] is the actual character
-  // Add that character to the password
-
-  // Once we finish the for loop, return the generated password
-// }
 
 /// Function to prompt user for password options
 function getPasswordOptions() {
-  let length = parseInt(prompt('Choose a password length (between 8 and 128 characters)'));
-  
+  let length = parseInt(
+    prompt("Choose a password length (between 8 and 128 characters)")
+  ); //Prompt the user to choose the length of the password
+
   if (isNaN(length) || length < 8 || length > 128) {
-    alert('Invalid length. Please choose a length between 8 and 128 characters.');
+    alert(
+      "Invalid length. Please choose a length between 8 and 128 characters."
+    ); //Show an alert if the length is not within the specified range or is not a number.
     return null;
   }
-    //These properties correspond to the user's selections regarding including specific character types in the generated password. Each of these properties holds a boolean value (true or false) based on the user's choice to include or exclude that particular character type.
-  let userspecialCharacters = confirm('Include special characters in the password?');
-  let usernumericCharacters = confirm('Include numeric characters in the password?');
-  let userlowerCasedCharacters = confirm('Include lowercase characters in the password?');
-  let userupperCasedCharacters = confirm('Include uppercase characters in the password?');
+  //These properties correspond to the user's selections regarding including specific character types in the generated password. Each of these properties holds a boolean value (true or false) based on the user's choice to include or exclude that particular character type.
+  let userspecialCharacters = confirm(
+    "Would you like to include special characters in the password?"
+  );
+  let usernumericCharacters = confirm(
+    "Would you like to include numeric characters in the password?"
+  );
+  let userlowerCasedCharacters = confirm(
+    "Would you like to include lowercase characters in the password?"
+  );
+  let userupperCasedCharacters = confirm(
+    "Would you like to include uppercase characters in the password?"
+  );
 
-  if (!(userspecialCharacters || usernumericCharacters || userlowerCasedCharacters || userupperCasedCharacters)) {
-    alert('Please select at least one character type.');
+  // If the user doesn't select any option, an alert will be shown.
+  if (
+    !(
+      userspecialCharacters ||
+      usernumericCharacters ||
+      userlowerCasedCharacters ||
+      userupperCasedCharacters
+    )
+  ) {
+    alert("Please select at least one character type.");
     return null;
+  }
+
+  //user can confirm all his option chosen
+  if ( // If the chosen length falls within the specified range and at least one character type is selected, it displays this confirmation.
+    userspecialCharacters ||
+    usernumericCharacters ||
+    userlowerCasedCharacters ||
+    userupperCasedCharacters
+  ) {
+    const message =
+      "You chose length: " +
+      length +
+      "\n" +
+      "You inclued special characters: " +
+      userspecialCharacters +
+      "\n" +
+      "You inclued numeric characters: " +
+      usernumericCharacters +
+      "\n" +
+      "You inclued lowerCase characters: " +
+      userlowerCasedCharacters +
+      "\n" +
+      "You inclued upperCase characters: " +
+      userupperCasedCharacters +
+      "\n";
+
+    confirm(message);
   }
 
   return {
@@ -167,30 +163,28 @@ function getPasswordOptions() {
     userupperCasedCharacters: userupperCasedCharacters,
   };
 }
-
+// Function for getting a random element from an array
 function generatePassword(options) {
-  let characters = ''; // Initializes an empty string variable named characters to store all the characters that can be used to generate the password.
-  
+  let characters = ""; // Initializes an empty string variable named characters to store all the characters that can be used to generate the password.
+
   //If the user chooses to include special characters (true), it concatenates the specialCharacters array elements into the characters string using join(''). This means that all special characters are concatenated together without any separator.
-  if (options.userspecialCharacters) { 
-    characters += specialCharacters.join('');
+  if (options.userspecialCharacters) {
+    characters += specialCharacters.join("");
   }
   if (options.usernumericCharacters) {
-    characters += numericCharacters.join('');
+    characters += numericCharacters.join("");
   }
   if (options.userlowerCasedCharacters) {
-    characters += lowerCasedCharacters.join('');
+    characters += lowerCasedCharacters.join("");
   }
   if (options.userupperCasedCharacters) {
-    characters += upperCasedCharacters.join('');
+    characters += upperCasedCharacters.join("");
   }
-
-  let password = '';
+  let password = ""; // create a variable to store the random password on it
   for (let i = 0; i < options.length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters.charAt(randomIndex);
+    password += characters.charAt(randomIndex); //Get a random characther
   }
-
   return password;
 }
 
@@ -198,15 +192,15 @@ function generatePassword(options) {
 function writePassword() {
   var passwordLength = getPasswordOptions(); // create a variable to store the function on it and then I get my length to use in my password variable below
   if (passwordLength !== null) {
-    var password = generatePassword(passwordLength); 
-    
-    var passwordText = document.querySelector('#password');
-    passwordText.value = password;
+    //It means that the user has selected at least one type of character and the chosen length for the password falls within the specified range
+    var password = generatePassword(passwordLength); //f the user provided valid options (i.e., passwordLength is not null), it calls the generatePassword() function to create a password using the provided options.
+    var passwordText = document.querySelector("#password"); //It then selects the HTML element with the ID #password (<input> element, presumably) using document.querySelector('#password').
+    passwordText.value = password; //The generated password is assigned to the value property of this HTML element, making it visible in the input field on the webpage.
   }
 }
 
 // Get reference to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate"); //selects an HTML element with the ID #generate.
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword); //When the btn is clicked triggers the 'writePassword()' function and shows the password
